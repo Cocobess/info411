fa98b97abe358a7d64b3d69b83857b50b1f477b0
Cocobess <corentinbesson.74@gmail.com>
2025-03-18 13:57:20 +0100

    modification pour bonne arborescance

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>WikiFruit</title>
-  <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
 
@@ -11,6 +11,7 @@
 
 
 <h2>Les fruits les plus chaud de ton departement</h2>
+
 
 </body>
 </html>
