b6285aa55f44980f1627cae0d9f2a48ddcdbd654
Cocobess <corentinbesson.74@gmail.com>
2025-03-18 14:22:05 +0100

    ajout des fichiers sql avec bon fichier de connexion a la bdd

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # info411
 Projet de TP d'info 411
+
+Mise en place d'un site web afin de gerer ensuite son implementation sur un serveur web. 
+Pour travaillé les competences en administrateur web.
+
+Besson Corentin && Marzouk Riyad
--- include/connexion.php
+++ include/connexion.php
@@ -1,8 +1,20 @@
 <?php
 $conn = mysqli_connect ("localhost" , "root" , "" , "info411");
 
-if(!$conn){
-    echo("something wrong!!!") ; 
+//Connexion au serveur de bases de données
+if (mysqli_connect_errno()) {
+    echo 'Désolé, connexion au serveur ' . "localhost" . ' impossible, '. mysqli_connect_error(), "\n";
+    exit();
+}
+// Sélection de la base de données
+mysqli_select_db($conn, "info411");
+if (mysqli_connect_errno()) {
+    echo 'Désolé, accès à la base ' . "info411" . ' impossible, '. mysqli_connect_error(), "\n";
+    exit();
+}
+// Spécification de l'encodage UTF-8 pour dialoguer avec la BD
+if (!mysqli_set_charset($conn, 'UTF8')) {
+    echo 'Erreur au chargement de l\'encodage UTF-8 : ', mysqli_connect_error(), "\n";
 }
 
 
